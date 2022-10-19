ba36845c47c3fd11fd62e0db30825c74d8d42ef1
Wei-Tse Sun <wtse.sun@gmail.com>
2022-10-18 21:10:40 -0700

    create enter alter terminal

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,11 @@
-use std::error::Error;
+use crossterm::{
+    cursor::{Hide, Show},
+    terminal::{self, EnterAlternateScreen, LeaveAlternateScreen},
+    ExecutableCommand,
+};
 use rusty_audio::Audio;
+use std::error::Error;
+use std::io;
 
 fn main() -> Result<(), Box<dyn Error>> {
     let mut audio = Audio::new();
@@ -11,7 +17,16 @@ fn main() -> Result<(), Box<dyn Error>> {
     audio.add("win", "sounds/win.wav");
     audio.play("startup");
 
+    // Terminal
+    let mut stdout = io::stdout();
+    terminal::enable_raw_mode()?;
+    stdout.execute(EnterAlternateScreen)?;
+    stdout.execute(Hide)?;
+
     // Cleanup the sounds
     audio.wait();
+    stdout.execute(Show)?;
+    stdout.execute(LeaveAlternateScreen)?;
+    terminal::disable_raw_mode()?;
     Ok(())
 }
