02ccb428e2508a4ca9b2e23c47d7d6c56cc07ce2
Wei-Tse Sun <wtse.sun@gmail.com>
2022-10-30 20:46:34 -0700

    add win and lose condition

--- src/main.rs
+++ src/main.rs
@@ -80,10 +80,11 @@ fn main() -> Result<(), Box<dyn Error>> {
         if invaders.update(delta) {
             audio.play("move");
         }
+        if player.detect_hits(&mut invaders) {
+            audio.play("explode");
+        }
 
         // Draw & render
-        // player.draw(&mut curr_frame);
-        // invaders.draw(&mut curr_frame);
         let drawables: Vec<&dyn Drawable> = vec![&player, &invaders];
         for drawable in drawables {
             drawable.draw(&mut curr_frame);
@@ -91,6 +92,16 @@ fn main() -> Result<(), Box<dyn Error>> {
 
         let _ = render_tx.send(curr_frame);
         thread::sleep(Duration::from_millis(1));
+
+        // Win or lose
+        if invaders.all_killed() {
+            audio.play("win");
+            break 'gameloop;
+        }
+        if invaders.reached_bottom() {
+            audio.play("lose");
+            break 'gameloop;
+        }
     }
 
     // Cleanup the sounds
